2634366e91e5bf825d2d86db8c393afb896717c4
wariox3 <maestradaz3@gmail.com>
2026-05-15 16:30:30 -0500

    feat: Add lista_usuario action to CtnContactoViewSet for retrieving user contacts

--- contenedor/views/contacto.py
+++ contenedor/views/contacto.py
@@ -1,5 +1,6 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
 from contenedor.models import CtnContacto
@@ -18,3 +19,16 @@ class CtnContactoViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
+
+    @extend_schema(
+        summary='Contactos del usuario autenticado',
+        description='Retorna los contactos creados por el usuario autenticado.',
+        responses=CtnContactoSerializer(many=True),
+    )
+    @action(detail=False, methods=['get'], url_path='lista-usuario')
+    def lista_usuario(self, request):
+        qs = CtnContacto.objects.filter(
+            usuario=request.user,
+        ).select_related('identificacion', 'ciudad', 'usuario')
+        pagina = self.paginate_queryset(qs)
+        return self.get_paginated_response(CtnContactoSerializer(pagina, many=True).data)
