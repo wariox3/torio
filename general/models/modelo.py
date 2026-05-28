ee3f46369e61cf8cc80ded00d6d5e24b11fce52a
wariox3 <maestradaz3@gmail.com>
2026-05-28 11:31:33 -0500

    feat: Add 'tipo' field to GenModelo with choices for better classification

--- general/models/modelo.py
+++ general/models/modelo.py
@@ -2,11 +2,22 @@ from django.db import models
 
 
 class GenModelo(models.Model):
+    class Tipo(models.TextChoices):
+        ADMINISTRADOR = 'A', 'Administrador'
+        MOVIMIENTO = 'M', 'Movimiento'
+        DETALLE = 'D', 'Detalle'
+
     id = models.BigIntegerField(primary_key=True)
     app = models.CharField(max_length=50)
     clase = models.CharField(max_length=50)
     nombre = models.CharField(max_length=100)
     tabla = models.CharField(max_length=100, null=True)
+    tipo = models.CharField(
+        max_length=1,
+        choices=Tipo.choices,
+        default=Tipo.ADMINISTRADOR,
+        db_default=Tipo.ADMINISTRADOR,
+    )
 
     class Meta:
         db_table = 'gen_modelo'
