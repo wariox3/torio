2148cb2a37131f977bd1a92ebcabb53366276956
wariox3 <maestradaz3@gmail.com>
2026-06-20 14:21:23 -0500

    feat: Add documento_detalle_afectado_id field to GenDocumentoDetalleSerializer

--- general/serializers/documento_detalle.py
+++ general/serializers/documento_detalle.py
@@ -21,13 +21,14 @@ class GenDocumentoImpuestoSerializer(serializers.ModelSerializer):
 
 
 class GenDocumentoDetalleSerializer(serializers.ModelSerializer):
-    campos_filtrables = {'id', 'documento_id', 'item_id', 'tipo_registro', 'cuenta_id', 'contacto_id', 'contacto__nombre_corto', 'contacto__numero_identificacion', 'modalidad_id', 'afectado', 'pendiente'}
+    campos_filtrables = {'id', 'documento_id', 'documento_detalle_afectado_id', 'item_id', 'tipo_registro', 'cuenta_id', 'contacto_id', 'contacto__nombre_corto', 'contacto__numero_identificacion', 'modalidad_id', 'afectado', 'pendiente'}
     select_related_lista = ('item', 'modalidad', 'cuenta', 'contacto', 'puesto')
     ordenamiento_default_lista = ('-id',)
 
     documento = serializers.PrimaryKeyRelatedField(
         queryset=GenDocumento.objects.all(), required=False,
     )
+    documento_detalle_afectado_id = serializers.IntegerField(read_only=True)
     item_nombre = serializers.CharField(source='item.nombre', read_only=True, default=None)
     modalidad_nombre = serializers.CharField(source='modalidad.nombre', read_only=True, default=None)
     modalidad_codigo = serializers.CharField(source='modalidad.codigo', read_only=True, default=None)
@@ -59,6 +60,7 @@ class GenDocumentoDetalleSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'documento',
+            'documento_detalle_afectado_id',
             'tipo_registro',
             'cantidad',
             'precio',
