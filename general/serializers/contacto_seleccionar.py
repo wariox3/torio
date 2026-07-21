b38b4188b0349060862f840d922f6c7e86947727
MarioA <maestradaz3@gmail.com>
2026-07-21 15:17:39 -0500

    feat: Add 'plazo_pago_dias' and 'plazo_pago_proveedor_dias' fields to GenContactoSeleccionarSerializer

--- general/serializers/contacto_seleccionar.py
+++ general/serializers/contacto_seleccionar.py
@@ -4,6 +4,17 @@ from general.models import GenContacto
 
 
 class GenContactoSeleccionarSerializer(serializers.ModelSerializer):
+    ciudad_nombre = serializers.CharField(source='ciudad.nombre', read_only=True)
+    plazo_pago_dias = serializers.IntegerField(source='plazo_pago.dias', read_only=True, default=None)
+    plazo_pago_proveedor_dias = serializers.IntegerField(
+        source='plazo_pago_proveedor.dias', read_only=True, default=None,
+    )
+
     class Meta:
         model = GenContacto
-        fields = ['id', 'nombre_corto', 'numero_identificacion']
+        fields = [
+            'id', 'nombre_corto', 'numero_identificacion',
+            'direccion', 'correo', 'ciudad', 'ciudad_nombre', 'telefono',
+            'precio_id', 'plazo_pago_id', 'plazo_pago_proveedor_id',
+            'plazo_pago_dias', 'plazo_pago_proveedor_dias',
+        ]
